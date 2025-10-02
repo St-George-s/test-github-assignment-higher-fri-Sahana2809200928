4f97a69a934017e2ff98e45a6212fc7e107a0edf
Sahana2809200928 <siv001@stge.org.uk>
2025-10-02 09:17:42 +0000

    endoftest

--- Software/test/test.py
+++ Software/test/test.py
@@ -5,24 +5,31 @@ def readGameDatafromCSV():
         gameTitles = []
         genres = []
         ageRatings = []
+        Platform= []
+
         reader = csv.reader(file) 
         next(reader)
         for row in reader:
             gameTitles.append(row[0])
             genres.append(row[1])
             ageRatings.append(row[2])
+            Platform.append(row[3])
 
             return(gameTitles, genres, ageRatings)
 
 def countSuitableGames(genre_to_check):
     count = 0
-    for index in global_gameTitles: 
-        if global_genres[index] == genre_to_check and global_ageRatings[0] <18:
-            print(global_gameTitles[index])
-            count= count +1
-    print(count)
+    with open("Software/test/platform_suitable_games.txt", "w") as file:
+        for index in global_gameTitles: 
+            if global_genres[index] == genre_to_check and global_ageRatings[0] <18:
+                print(global_gameTitles[index])
+                count= count +1
+                file.write(global_gameTitles[index], global_Platform[index])
+        print(count)
+    
 
 #main
 
-global_gameTitles, global_genres, global_ageRatings = readGameDatafromCSV()
-countSuitableGames()
+global_gameTitles, global_genres, global_ageRatings, global_Platform = readGameDatafromCSV()
+countSuitableGames("Fantasy")
+
