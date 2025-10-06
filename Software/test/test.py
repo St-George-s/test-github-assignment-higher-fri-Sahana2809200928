3c140f475475d35765a00fd1c7720372ab336580
Sahana2809200928 <siv001@stge.org.uk>
2025-10-06 14:50:59 +0000

    done

--- Software/test/test.py
+++ Software/test/test.py
@@ -5,31 +5,37 @@ def readGameDatafromCSV():
         gameTitles = []
         genres = []
         ageRatings = []
-        Platform= []
+        platform= []
 
         reader = csv.reader(file) 
-        next(reader)
         for row in reader:
             gameTitles.append(row[0])
             genres.append(row[1])
             ageRatings.append(row[2])
-            Platform.append(row[3])
+            platform.append(row[3])
 
-            return(gameTitles, genres, ageRatings, Platform)
+            return gameTitles, genres, ageRatings, platform
 
-def countSuitableGames(genre_to_check):
+# Part B
+def countSuitableGames(gameTitles, genres, ageRatings, genre_to_check):
     count = 0
-    with open("Software/test/platform_suitable_games.txt", "w") as file:
-        for index in global_gameTitles: 
-            if global_genres[index] == genre_to_check and global_ageRatings[0] <18:
-                print(global_gameTitles[index])
-                count= count +1
-                file.write(global_gameTitles[index], global_Platform[index])
-        print(count)
+    for counter in range (len(gameTitles)): 
+        if genres[counter] == genre_to_check and ageRatings[counter] <18:
+            count= count +1
+    print(count)
+        
+# Part C
+# def countSuitableGames(gameTitles, genres, ageRatings, genre_to_check):
+#     count = 0
+#     with open("Software/test/platform_suitable_games.txt", "w") as file:
+#         for index in gameTitles: 
+#             if genres[index] == genre_to_check and ageRatings[0] <18:
+#                 count= count +1
+#                 file.write(gameTitles[index], global_Platform[index])
     
 
 #main
 
-global_gameTitles, global_genres, global_ageRatings, global_Platform = readGameDatafromCSV()
-countSuitableGames("Fantasy")
-
+# globa_gameTitles, global_genres, global_ageRatings, global_Platform = readGameDatafromCSV()
+# countSuitableGames(globa_gameTitles,  global_genres, global_ageRatings,"Fantasy")
+countSuitableGames(["GTA 6", "Assasin's creed"], ["Action", "Action"], [18, 18],"Action")
