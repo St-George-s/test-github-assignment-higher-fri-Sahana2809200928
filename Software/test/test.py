2ddf4a47c48ec18d7b23d30a5b37890d139aa63f
Sahana2809200928 <siv001@stge.org.uk>
2025-10-11 22:18:40 +0000

    2022 CW

--- Software/test/test.py
+++ Software/test/test.py
@@ -2,13 +2,14 @@ import csv
 
 def readGameDatafromCSV():
     with open("Software/test/gamesExtended.csv", "r") as file:
+        #parallel arrays
         gameTitles = []
         genres = []
         ageRatings = []
         platforms= []
-
+        #reader is equal to whole CSV file
         reader = csv.reader(file) 
-        for row in reader:
+        for row in reader: #loop through each row which are made int eh sam format, add each component into a parallelarrays
             gameTitles.append(row[0])
             genres.append(row[1])
             ageRatings.append(int(row[2]))
@@ -20,9 +21,9 @@ def readGameDatafromCSV():
 def countSuitableGames(gameTitles, genres, ageRatings, genre_to_check):
     count = 0
     for index in range (len(gameTitles)): 
-        if genres[index] == genre_to_check and ageRatings[index] <18:
+        if genres[index] == genre_to_check and ageRatings[index] <18: #it is assumed that the CSSV is in parallel arrays, and that attributes that belong to same game.
             count= count +1
-    print(count)
+    print(count) #after for loop, how many games meet conditions
         
 #Part C
 def countSuitableGames(gameTitles, genres, ageRatings, genre_to_check, platform_to_check, platforms):
