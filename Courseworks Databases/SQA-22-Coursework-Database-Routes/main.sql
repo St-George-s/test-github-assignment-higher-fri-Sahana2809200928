3637f4f9ea22b7c04c215d1c3c0d900b716250d3
Sahana2809200928 <siv001@stge.org.uk>
2025-11-20 10:21:11 +0000

    done

--- Courseworks Databases/SQA-22-Coursework-Database-Routes/main.sql
+++ Courseworks Databases/SQA-22-Coursework-Database-Routes/main.sql
@@ -16,10 +16,30 @@ SELECT*
 FROM Walk
 LIMIT 2;
 
-
+--2B
 SELECT P.forename, P.surname, P.plannerNo, count(W.WalkID)
 FROM Planner P
 JOIN Route R ON P.plannerNo = R.plannerNo
 JOIN Walk W ON W.routeID = R.routeID
 GROUP BY P.plannerNo
 ORDER BY count(W.WalkID) DESC;
+
+--2C
+SELECT Wal.walkerNo, Wal.forename, Wal.surname, Wal.telNo
+FROM Walker Wal
+JOIN Walk Wa ON Wa.walkerNo = Wal.walkerNo
+JOIN Route R ON R.routeID = Wa.routeID
+WHERE R.distance = (
+    SELECT MAX(Distance)
+    FROM Route
+)
+GROUP BY Wa.walkerNo;
+
+SELECT R.routeID, woodName, description
+FROM Route R
+
+]
+WHERE R.footwear LIKE '%shoe%';
+
+
+
